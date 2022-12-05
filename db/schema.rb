75ea162235ad6b2fcc28cc59f79a138e3f62d1f1
TiagoR79 <Tiagor79@protonmail.com>
2022-12-05 15:22:28 +0000

    Created UserStock Table and associations
    Added stocks to db and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_155809) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_150412) do
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "stock_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_155809) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_stocks", "stocks"
+  add_foreign_key "user_stocks", "users"
 end
