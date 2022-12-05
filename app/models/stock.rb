fd3da9818a237e03fbfd1b566feffd16cf4c99a6
TiagoR79 <Tiagor79@protonmail.com>
2022-12-05 11:17:40 +0000

    Dealing with invalid search results
    Output is not the desired as it should return a error message but
    instead it returs the price at 0 for invalid Symbold
    Blank Search is working as intended

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,14 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker)
     client = Alphavantage::Client.new
-
-    new(
-      ticker: ticker,
-      price: client.quote(ticker)["Global Quote"]["05. price"].to_i
-    )
+    
+    begin
+      new(
+        ticker: ticker,
+        price: client.quote(ticker)["Global Quote"]["05. price"].to_f
+      )
+    rescue => exception
+      return nil
+    end
   end
 end
